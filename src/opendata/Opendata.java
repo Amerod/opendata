364d2b36fc6b0d31660151c071b83096f02e3602
Amerod <amerod95@gmail.com>
2015-06-12 22:57:42 +0200

    Proyecto para importar csv a base de datos.

---
+++ src/opendata/Opendata.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opendata;
+import java.io.*;
+/**
+ *
+ * @author andres
+ */
+public class Opendata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File archivo = null;
+        FileReader lee = null;
+        BufferedReader br = null;
+        // TODO code application logic here
+        try{
+        archivo = new File("D:\\a\\datos.csv");
+        lee = new FileReader (archivo);
+        br = new BufferedReader (lee);
+        while(br.readLine() != null){
+        System.out.println(br.readLine());
+        System.out.println();
+        }
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+        try{
+            lee.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        }
+    }
+    
+}
