70791adbe96650d5a8d6f6172fffd80834c5d1ee
Amerod <amerod95@gmail.com>
2015-06-15 21:00:16 +0200

    Proyecto para importar csv a base de datos.

--- src/opendata/Opendata.java
+++ src/opendata/Opendata.java
@@ -1,11 +1,42 @@
 package opendata;
 import java.io.*;
+import opendata.modelo.Agenda;
 /**
  *
  * @author andres
  */
 public class Opendata {
-
+    
+    public static Agenda crear_entrada(BufferedReader br){
+        String linea = "";
+        String separador = ";";
+        String[] datos = null;
+        Agenda entrada = null;
+        try{
+            linea = br.readLine();
+            datos = linea.split(separador);
+            if (datos[0].equals("﻿ID")){
+               linea = br.readLine();
+               datos = linea.split(separador);
+            };
+            entrada = new Agenda(datos);
+        } catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            return entrada;
+        }
+    };
+    
+    public static void mostrar_entrada(Agenda entrada){
+       // System.out.println("-- Datos de la entrada -- ");
+        System.out.println("ID : "+entrada.getID());
+      /*  System.out.println("Nombre : "+entrada.getActe());
+        System.out.println("Descripcion : "+entrada.getDescripcio());
+        System.out.println("Lugar : "+entrada.getLloc());
+        System.out.println("Fecha : "+entrada.getData_ini());
+        System.out.println("Distrito : "+entrada.getDistricte());
+        System.out.println("URL : "+entrada.getURL());*/
+    };
     /**
      * @param args the command line arguments
      */
@@ -13,23 +44,18 @@ public class Opendata {
         File archivo = null;
         FileReader lee = null;
         BufferedReader br = null;
-        String linea = "";
-        String separador = ";";
-        // TODO code application logic here
         try{
-            archivo = new File("src\\datos.csv");
-            lee = new FileReader (archivo);
-            br = new BufferedReader (lee);
-            while((linea = br.readLine()) != null){
-            String[] datos = linea.split(separador);
-            if (!datos[0].equals("﻿ID")){
-                   System.out.println(datos[0]);
-            };
-            };
-        } catch(Exception e){
-            e.printStackTrace();
+        archivo = new File("src\\datos.csv");
+        lee = new FileReader(archivo);
+        br = new BufferedReader (lee);
+        Agenda entrada = crear_entrada(br);
+        while (entrada != null){
+        mostrar_entrada(entrada); 
+        entrada = crear_entrada(br);
         }
-        finally{
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
             try{
                 lee.close();
             }
---
+++ src/opendata/modelo/Agenda.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opendata.modelo;
+
+
+/**
+ *
+ * @author andres
+ */
+public class Agenda {
+    int ID;
+    String acte;
+    String descripcio;
+    String lloc;
+    String data_ini;
+    String districte;
+    String URL;
+
+    public Agenda(String[] datos) {
+        this.ID = Integer.parseInt(datos[0]);
+        this.acte = datos[1];
+        this.descripcio = datos[2];
+        this.lloc = datos[6];
+        this.data_ini = datos[7];
+        this.districte = datos[14];
+        this.URL = datos[26];
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getActe() {
+        return acte;
+    }
+
+    public void setActe(String acte) {
+        this.acte = acte;
+    }
+
+    public String getDescripcio() {
+        return descripcio;
+    }
+
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
+
+    public String getLloc() {
+        return lloc;
+    }
+
+    public void setLloc(String lloc) {
+        this.lloc = lloc;
+    }
+
+    public String getData_ini() {
+        return data_ini;
+    }
+
+    public void setData_ini(String Data_ini) {
+        this.data_ini = Data_ini;
+    }
+
+    public String getDistricte() {
+        return districte;
+    }
+
+    public void setDistricte(String Districte) {
+        this.districte = Districte;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+    
+}
